48d1425ef81beb53445796e0e78c0720d962d7be
Haziq <haziqabdrshd@gmail.com>
2022-06-29 13:26:49 +0800

    Update script.js
    Display the number of articles for each category.

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ $(document).ready(function() {
 		url: 'https://62b91ad341bf319d22761d85.mockapi.io/api/v1/objects/articles',
 		type: "GET",
 		success: function (result) {
-			console.log(result)
+			var test = _.countBy(result, 'category');
+			_.forEach(test, function(value,category) {
+				document.write(category, "&nbsp;" + value + "<br>");
+			})
+			
 		},
 		error: function(err) {
 			console.log(err);
